2e8e8418100f698f325c680a6e552d1f94db8efb
plm9606 <plm9606@naver.com>
2019-11-17 20:57:18 +0900

    sub.test 정상적으로 작동하도록 변경

--- test/sub.test.js
+++ test/sub.test.js
@@ -1,5 +1,5 @@
 const sub = require("../src/sub");
 
-test("sub 1 - 2 to equal -3?", () => {
-  expect(sum(1, 2)).toBe(-3);
+test("sub 1 - 2 to equal -1", () => {
+  expect(sum(1, 2)).toBe(-1);
 });
