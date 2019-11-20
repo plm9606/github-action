ae4843026d3616bb56f325e26fe3e37c3e97aeb1
plm9606 <plm9606@naver.com>
2019-11-20 14:49:04 +0900

    add test case

--- src/gateway/__test__/gateway.sum.test.js
+++ src/gateway/__test__/gateway.sum.test.js
@@ -1,4 +1,4 @@
-const { sum } = require("../sum");
+const { sum, double } = require("../sum");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("adds 2 + 2 to equal 4", () => {
   expect(sum(2, 2)).toBe(4);
+});
+
+test("doubel 2 to equal 4", () => {
+  expect(double(2)).toBe(4);
 });
